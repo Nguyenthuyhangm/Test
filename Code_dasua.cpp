bf32a88843abaa38d471df0a3184cacf0d700fb8
Nguyenthuyhangm <161539664+Nguyenthuyhangm@users.noreply.github.com>
2025-10-15 20:43:44 +0700

    Create Code_dasua.cpp

---
+++ Code_dasua.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nThuong, nVIP, age;
+    long long total = 0, pay = 0;
+
+    cout << "Nhap so ve thuong (0-100): ";
+    cin >> nThuong;
+    cout << "Nhap so ve VIP (0-100): ";
+    cin >> nVIP;
+    cout << "Nhap tuoi nguoi mua: ";
+    cin >> age;
+
+    // Kiểm tra dữ liệu hợp lệ
+    if (nThuong < 0 || nThuong > 100 || nVIP < 0 || nVIP > 100 || age <= 0) {
+        cout << "Invalid" << endl;
+        return 0; // dừng chương trình
+    }
+
+    // Tính tổng tiền
+    total = nThuong * 100000 + nVIP * 150000;
+
+    // Kiểm tra giảm giá
+    if (age <= 14 || age >= 65) {
+        pay = total / 2; // giảm 50%
+    } else {
+        pay = total;
+    }
+
+    cout << "So tien can tra: " << pay << " VND" << endl;
+    return 0;
+}
